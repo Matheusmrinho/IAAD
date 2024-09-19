272f19ca0e0dbc1e88d52c8eddda992611fa5d5e
Gabriel da Silva <137402778+Gabrielsilva090@users.noreply.github.com>
2024-09-19 08:38:34 -0300

    tentando implementar diretor

--- src/app.py
+++ src/app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 from crud import insert_filme, get_filmes, update_filme, delete_filme, get_filmes_recentes, get_filmes_filtrado, get_filmes_por_ano
 from db_connect import connect_db 
-import plotly.graph_objects as go
 
 # Conectando ao banco de dados
 conn = connect_db()
@@ -13,36 +12,27 @@ st.title("Sistema de Programação de Filmes 🎬")
 st.markdown("""
     <style>
         /* Cor de fundo do app */
-        .main { background-color: #ffffff; }
+        .main { background-color: #000000; }
 
         /* Títulos */
-        h1, h2, h3 { color: #FF0000; }
+        h1, h2, h3 { color: #FFFFFF; }
             
         /* Botões */
         .stButton>button { background-color: #FFD700; color: #000000; border: none; }
-        .stButton>button:hover { background-color: #FFC107; }
+        .stButton>button:hover { background-color: #FFFFFF; color: #000000; }
 
         /* Painéis de dados */
-        .stDataFrame>div { background-color: #ffffff; border-radius: 5px; padding: 10px; }
         .stDataFrame>div>div { border: none; } 
-
-        /* Resultados dos filmes recentes */
-        .result-table { background-color: #f0f0f0; padding: 15px; border-radius: 10px; text-align: center; }
-        .result-table th { background-color: #FFD700; color: #000000; text-align: center; }
-        .result-table td { color: #000000; text-align: center; }
 
         /* Mensagens de sucesso */
         .stSuccess { color: #00FF00; font-weight: bold; }
 
-        /* Estilo das colunas */
-        .stColumn { padding: 10px; }
-
         /* Páginas CRUD */
-        .crud-page { background-color: #ffffff; padding: 15px; border-radius: 10px; }
-        .crud-page h3 { color: #FF0000; }
-        .crud-page .stButton>button { background-color: #FF0000; color: #ffffff; }
-        .crud-page .stButton>button:hover { background-color: #CC0000; }
-
+        .crud-page { 
+            background-color: #ffffff; 
+            border-radius: 10px;
+        }
+        
         /* Cor do texto dentro das caixas de input */
         .stTextInput input, .stNumberInput input, .stSelectbox select {
             color: #FFFFFF;  /* Texto branco dentro das caixas */
@@ -50,11 +40,30 @@ st.markdown("""
 
         /* Cor dos labels acima das caixas de input */
         .stTextInput label, .stNumberInput label, .stSelectbox label {
-            color: #000000;  /* Labels pretos acima das caixas */
+            color: #FFFFFF;  /* Labels brancos acima das caixas */
             font-weight: bold;
+        }
+
+        /* Ajuste de margens do conteúdo */
+        form {
+            margin-top: 0;  /* Elimina o espaçamento acima do formulário */
+            margin-bottom: 0; /* Elimina o espaçamento abaixo do formulário */
+        }
+
+        /* Remover estilização da tabela de filmes recentemente adicionados */
+        .result-table {
+            background-color: initial; /* Remove o fundo personalizado */
+            padding: 0; /* Remove o padding */
+            border-radius: 0; /* Remove o arredondamento */
+        }
+        .result-table th, .result-table td {
+            background-color: initial; /* Remove o fundo das células */
+            color: initial; /* Remove a cor personalizada do texto */
         }
     </style>
 """, unsafe_allow_html=True)
+
+
 
 # Inicializando o estado da página
 if 'current_page' not in st.session_state:
@@ -69,26 +78,21 @@ if st.session_state.current_page == "Início":
     st.subheader("Filmes Recentemente Adicionados")
     
     # Exibindo filmes recentes
-    filmes_recentes = get_filmes_recentes(conn)  
-    df = pd.DataFrame(filmes_recentes, columns=["Número", "Título Original", "Título Brasil", "Ano", "País", "Categoria", "Duração", "Coluna Extra"])
+    filmes_recentes = get_filmes_recentes(conn)
+    df = pd.DataFrame(filmes_recentes, columns=["Número", "Título Original", "Título Brasil", "Ano", "País", "Categoria", "Duração", "Diretor", "Coluna Extra"])
     html = df.to_html(index=False, classes='result-table')
     st.markdown(html, unsafe_allow_html=True)
 
-     # Gráfico de filmes por ano
-    st.subheader("Filmes por Ano")
-    
-    # Obter dados para o gráfico
-    filmes_por_ano = get_filmes_por_ano(conn)
-    
-    # Transformar em DataFrame
-    df_ano = pd.DataFrame(filmes_por_ano, columns=["Ano", "Total de Filmes"])
-    
-    # Exibir gráfico de barras
-    st.bar_chart(df_ano.set_index('Ano'))
+
+    st.subheader("Extras")
+    col1, = st.columns(1)
+    with col1:
+        if st.button("Gráficos ➡️"):
+            change_page("Gráficos")
     
     # Exibindo botões para navegação entre os CRUD
     st.subheader("Ações CRUD")
-    col1, col2, col3, col4, col5 = st.columns(5)
+    col1, col2, col3, col4 = st.columns(4)
 
     with col1:
         if st.button("Cadastrar Filme ➕"):
@@ -99,14 +103,10 @@ if st.session_state.current_page == "Início":
             change_page("Consultar Filmes")
 
     with col3:
-        if st.button("Modo pesquisa 🔍"):
-            change_page("Modo Pesquisa")
-
-    with col4:
         if st.button("Atualizar Filme ✏️"):
             change_page("Atualizar Filme")
 
-    with col5:
+    with col4:
         if st.button("Remover Filme 🗑️"):
             change_page("Remover Filme")
 
@@ -121,11 +121,12 @@ elif st.session_state.current_page == "Cadastrar Filme":
         titulo_brasil = st.text_input("Título no Brasil")
         ano_lancamento = st.number_input("Ano de Lançamento", min_value=1800, max_value=2024)
         pais_origem = st.text_input("País de Origem")
-        categoria = st.selectbox("Categoria", ["Animação", "Drama", "Comédia", "Romance", "Ação", "Ficção Científica"])
+        categoria = st.selectbox("Categoria", ["Animação", "Drama", "Comédia", "Romance", "Ação", "Ficção Científica", "Nome do Diretor"])
         duracao = st.number_input("Duração (minutos)", min_value=1)
+        num_diretor = st.text_input("Nome do Diretor")
 
         if st.form_submit_button("Cadastrar"):
-            insert_filme(num_filme, titulo_original, titulo_brasil, ano_lancamento, pais_origem, categoria, duracao)
+            insert_filme(num_filme, titulo_original, titulo_brasil, ano_lancamento, pais_origem, categoria, duracao, num_diretor)
             st.markdown(f"<div class='stSuccess'>Filme '{titulo_brasil}' cadastrado com sucesso!</div>", unsafe_allow_html=True)
     
     if st.button("Voltar"):
@@ -151,7 +152,7 @@ elif st.session_state.current_page == "Consultar Filmes":
     # Filtrar os filmes
     if src:
         filmes = get_filmes_filtrado(titulo, cat, ano, pais)
-        df = pd.DataFrame(filmes, columns=["Número", "Título Original", "Título Brasil", "Ano", "País", "Categoria", "Duração", "Coluna Adicional"])
+        df = pd.DataFrame(filmes, columns=["Número", "Título Original", "Título Brasil", "Ano", "País", "Categoria", "Duração","Diretor", "Coluna Adicional"])
         
         if df.empty:
             st.markdown("<div class='stSuccess'>Nenhum filme encontrado com os filtros selecionados!</div>", unsafe_allow_html=True)
@@ -162,7 +163,7 @@ elif st.session_state.current_page == "Consultar Filmes":
     else:
         # Se não há pesquisa, exibir todos os filmes
         filmes = get_filmes()
-        df = pd.DataFrame(filmes, columns=["Número", "Título Original", "Título Brasil", "Ano", "País", "Categoria", "Duração", "Coluna Adicional"])
+        df = pd.DataFrame(filmes, columns=["Número", "Título Original", "Título Brasil", "Ano", "País", "Categoria", "Duração","Diretor", "Coluna Adicional"])
         st.dataframe(df)
 
     # Botão de voltar
@@ -211,33 +212,26 @@ elif st.session_state.current_page == "Remover Filme":
 
     st.markdown("</div>", unsafe_allow_html=True)
 
-elif st.session_state.current_page == "Modo Pesquisa":
-    st.markdown("<div class='crud-page'>", unsafe_allow_html=True)
-    st.subheader("Pesquisa de Filmes")
-    form = st.form('filme')
+elif st.session_state.current_page == "Gráficos":
+    st.subheader("Gráficos")
+    st.write("Aqui você pode adicionar gráficos personalizados")
 
-    # Adicionando filtros
-    with form:
-        titulo = st.text_input("Digite o título do filme")
-        cat = st.selectbox("Selecione a categoria", ["", "Animação", "Drama", "Comédia", "Romance", "Ação", "Ficção Científica"], placeholder='')
-        ano = st.number_input("Digite o ano de lançamento", min_value=1800, max_value=2024, value=None)
-        pais = st.selectbox("Selecione o país de origem", ["", "Brasil", "Estados Unidos", "França", "Itália", "Japão", "Reino Unido"], placeholder='')
-        src = st.form_submit_button("Pesquisar")
-
-    # Função para filtrar os filmes
-    if src:
-        filmes = get_filmes_filtrado(titulo, cat, ano, pais)
-        print(filmes)
-        df = pd.DataFrame(filmes, columns=["Número", "Título Original", "Título Brasil", "Ano", "País", "Categoria", "Duração", "Coluna Adicional"])
-        
-        if df.empty:
-            st.markdown("<div class='stSuccess'>Nenhum filme encontrado com os filtros selecionados!</div>", unsafe_allow_html=True)
-        else:
-            st.dataframe(df, height=300, width=800)
+     # Gráfico de filmes por ano
+    st.subheader("Filmes por Ano")
     
+    # Obter dados para o gráfico
+    filmes_por_ano = get_filmes_por_ano(conn)
+    
+    # Transformar em DataFrame
+    df_ano = pd.DataFrame(filmes_por_ano, columns=["Ano", "Total de Filmes"])
+    
+    # Exibir gráfico de barras
+    st.bar_chart(df_ano.set_index('Ano'))
     if st.button("Voltar"):
         change_page("Início")  # Botão para retornar à página inicial
 
     st.markdown("</div>", unsafe_allow_html=True)
+
+
 
 conn.close()
